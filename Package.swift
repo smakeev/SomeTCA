4fbd247ff51c7ef7c3f53168503951e84f333004
Sergey Makeev <>
2025-09-05 15:05:59 +0500

    More for structure

--- Package.swift
+++ Package.swift
@@ -1,3 +1,5 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
@@ -7,7 +9,7 @@ let package = Package(
         .library(name: "SomeTCA", targets: ["SomeTCA"])
     ],
     targets: [
-        .target(name: "SomeTCA", path: "Sources"),
+        .target(name: "SomeTCA", path: "SomeTCA"),
         .testTarget(name: "SomeTCATests", dependencies: ["SomeTCA"], path: "Tests")
     ]
 )
